905df62319eda1063a49660d57a173e6a1bf7953
Ajinkya <puar.ajinkya@gmail.com>
2018-02-18 12:10:09 +0530

    intercept request urls ending with .jpg and respond gifs

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,12 +2,17 @@ self.addEventListener('fetch', function (event) {
     // TODO: respond to all requests with an html response
     // containing an element with class="a-winner-is-me".
     // Ensure the Content-Type of the response is "text/html"
-    console.log(event.request);
-    event.respondWith(
-        new Response('<p class="a-winner-is-me">Hello</p>', {
-            headers: {
-                'Content-Type': 'text/html'
-            }
-        })
-    );
+    // console.log(event.request);
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+    // event.respondWith(
+    //     new Response('<p class="a-winner-is-me">Hello</p>', {
+    //         headers: {
+    //             'Content-Type': 'text/html'
+    //         }
+    //     })
+    // );
 });
