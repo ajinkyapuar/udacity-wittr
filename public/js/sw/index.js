a7b989bf9698c0b0ad7252761e0e4cc457a108e0
Ajinkya <puar.ajinkya@gmail.com>
2018-02-18 17:21:36 +0530

    theme changed, install new updated in new cache, delete old cache once service worker get updated

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,36 +1,38 @@
 self.addEventListener('install', function (event) {
-    var urlsToCache = [
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-    ];
-
     event.waitUntil(
-        // TODO: open a cache named 'wittr-static-v1'
-        // Add cache the urls from urlsToCache
-        caches.open('wittr-static-v1').then(function (cache) {
-            // console.log(cache);
-            cache.addAll(urlsToCache);
+        // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+        // Ensure at least $primary-color changes
+        // TODO: change cache name to 'wittr-static-v2'
+        caches.open('wittr-static-v2').then(function (cache) {
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        // TODO: remove the old cache
+        caches.delete('wittr-static-v1').then(function (res) {
+            console.log("Deleted previous cache");
 
         }).catch(function (err) {
-            // console.log(err);
+            console.log(err);
 
         })
     );
 });
 
 self.addEventListener('fetch', function (event) {
-    // TODO: respond with an entry from the cache if there is one.
-    // If there isn't, fetch from the network.
     event.respondWith(
-        caches.match(event.request).then(function (res) {
-            if (res)
-                return res;
-            else
-                return fetch(event.request.url);
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
         })
     );
 });
